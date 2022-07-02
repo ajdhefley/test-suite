011e2b971dc76e7a106b7ed726ab88ba5490f23a
Ashton Hefley <ajdhefley@gmail.com>
2022-07-02 17:38:44 -0500

    Remove packages

--- packages/test-suite-angular/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './angular-test-setup';
-export * from './angular-test-strategy';
-export * from './angular-test-suite';
--- packages/test-suite-core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './test-mock';
-export * from './test-mock-mapper';
-export * from './test-suite';
